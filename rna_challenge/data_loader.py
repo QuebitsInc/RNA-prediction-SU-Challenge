7e779ead8737d000b019f4a0a1a82e76166371fa
Quebits <quebits@gmail.com>
2023-11-24 14:22:00 +0500

    Dataloader / data classes added

---
+++ rna_challenge/data_loader.py
@@ -0,0 +1,14 @@
+from utils import dict_to
+
+
+class DeviceDataLoader:
+    def __init__(self, dataloader, device='cuda'):
+        self.dataloader = dataloader
+        self.device = device
+
+    def __len__(self):
+        return len(self.dataloader)
+
+    def __iter__(self):
+        for batch in self.dataloader:
+            yield tuple(dict_to(x, self.device) for x in batch)
